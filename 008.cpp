ce187ee39ff61cd32cf0ade75ceb6a350f920cf2
misuyama <144996653+misuyama@users.noreply.github.com>
2023-09-26 17:03:54 +0900

    Add files via upload

---
+++ 008.cpp
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+int main()
+{
+    int a[5][5];
+    int row, col;
+    scanf_s("%d %d", &row, &col);
+    
+
+
+    //이차원배열에 숫자를 입력받는다.
+
+    for (int i = 0; i < row; i = i + 1) {
+        for (int j = 0; j < col; j = j + 1)
+            scanf_s("%d %d", &row, &col);
+    }
+    printf("%d %d", row, col);
+
+    //출력
+    for (int i = 0; i < row; i = i + 1) {
+        for (int j = 0; j < col; j = j + 1)
+            printf("%d %d", row, col);
+        printf("\n");
+    }
+
+    //각 행의 요소의 합을 구하시오.
+
+    int sum = 0;
+    
+    for (int i = 0; i < row; i = i + 1) {
+        for (int j = 0; j < col; j = j + 1)
+            sum = sum + a[i][j];
+        printf("%d\n", sum);
+
+    }
+
+   
+
+}
