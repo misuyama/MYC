387aaad1b79758b7d9dd915d3be5e30cdcd096af
misuyama <144996653+misuyama@users.noreply.github.com>
2023-10-24 17:26:27 +0900

    피보나치 수열 2023 10 24

---
+++ 47.cpp
@@ -0,0 +1,22 @@
+﻿// 47.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    int a[21];
+
+    a[0] = a[1] = 1;
+
+    for (int i = 2; i < 20; i++) {
+        a[i] = a[i - 1] + a[i - 2];
+    }
+
+    for (int i = 0; i < 20 ; i++) {
+        printf("%d ", a[i]);
+
+    }
+    printf("\n");
+}
+
