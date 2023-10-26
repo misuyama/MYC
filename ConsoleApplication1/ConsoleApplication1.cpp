4cbdc8a2ae60f29cfe177aa7274ea4e3ed012e5a
misuyama <144996653+misuyama@users.noreply.github.com>
2023-10-26 10:46:56 +0900

    2023- 10 - 26 함수 재귀함수
    재귀함수 함수 매우 중요하다.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+void fact(int a) {
+	int f = 1;
+	for (int i = 1; i <= 10; i++)
+		f *= i;
+	return f;
+}
+
+int main() {
+	int n = fact(10);
+	printf("%d", n);
+
+}
+
---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+int add(int n) {
+	int a = 0;  //합 변수
+	for (int i = 1 ; i <= n ; i++) {
+		a = a + i; //합 변수
+	}
+	return a;
+}
+
+int recAdd(int n) {
+	if (n == 1)
+		return 1;
+	else
+		return recAdd(n - 1) + n;
+}
+
+int main() {
+	int n;
+
+	printf("n값을 입력: ");
+
+	scanf_s("%d", &n);
+
+	int sum = add(n);
+
+	printf("1 ~ %d까지의 합 = %d\n", n, sum);
+
+	int sum1 = recAdd(n);
+
+	printf("1 ~ %d까지의 합 = %d\n", n, sum1);
+
+
+
+}
